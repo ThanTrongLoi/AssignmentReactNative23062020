324f6048afe0dedc162c257ebe18ab628ce0640a
ThanTrongLoi <thantrongloi648@gmail.com>
2020-06-27 11:53:07 +0700

    Login Activity

--- App.js
+++ App.js
@@ -1,114 +1,136 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Text, 
+        View, 
+        ImageBackground,
+        Image,
+        StyleSheet,
+        TextInput,
+        Dimensions,
+        TouchableOpacity} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import bgImage from './images/background.jpg';
+import lgImage from './images/logo.png';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const { width: WIDTH } = Dimensions.get('window')
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+export default class loginActivity extends Component{
+  constructor() {
+    super()
+    this.state = {
+        showPass: true,
+        press: false,
+    }
+  }
+  showPass = () => {
+    if(this.state.press == false){
+      this.setState({ showPass: false, press: true })
+    }else{
+      this.setState({ showPass: true, press: false })
+    }
+  }
+
+  render(){
+    return(
+      <ImageBackground source = {bgImage} style = {styles.backgroundContainer}>
+          <View style = {styles.logoContainer}>
+            <Image style = {styles.logo} />
+            <Text style = {styles.textLogo}>React Native</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+
+          <View style = {styles.inputContainer}>
+            <Icon name = {'ios-person-outline'} size = {28} color = {'rgba(255, 255, 255, 0.7)'}
+                  style = {styles.inputIcon} />
+            <TextInput
+              style = {styles.input}
+              placeholder = {'Username'}
+              placeholderTextColor = {'rgba(255,255,255,0.7)'}
+              underlineColorAndroid = 'transparent'/>
+          </View>
+
+          <View style = {styles.inputContainer}> 
+            <Icon name = {'ios-lock-outline'} size = {28} color = {'rgba(255, 255, 255, 0.7)'}
+                  style = {styles.inputIcon} />
+
+            <TextInput
+              style = {styles.input}
+              placeholder = {'Password'}
+              secureTextEntry = {this.state.showPass}
+              placeholderTextColor = {'rgba(255,255,255,0.7)'}
+              underlineColorAndroid = 'transparent'/>
+
+            <TouchableOpacity style = {styles.btnEye}
+                onPress = {this.showPass.bind(this)}>
+              <Icon name = { this.state.press == false ? 'ios-eye-outline' : 'ios-eye-off-ouline'} size = {26} color = {'rgba(255, 255, 255, 0.7)'} />
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style = {styles.btnLogin}>
+            <Text style = {styles.textLogin}>Login</Text>
+          </TouchableOpacity>
+      </ImageBackground>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+  backgroundContainer: {
+    flex: 1,
+    width: null,
+    height: null,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50
+  },
+  logo: {
+    width: 120,
+    height:120
+  },
+  textLogo: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: "500",
+    marginTop: 10,
+    opacity: 0.5
+  },
+  inputContainer:{
+    marginTop: 10
+  },
+  input: {
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 25,
+    fontSize: 16,
+    paddingLeft: 45,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    color: 'rgba(255, 255, 255, 0.7)',
+    marginHorizontal: 25
+  },
+  inputIcon: {
+    position: 'absolute',
+    top: 8,
+    left: 37,
+  },
+  btnEye: {
+    position: 'absolute',
+    top: 8,
+    right: 37,
+  },
+  btnLogin: {
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: '#432577',
+    justifyContent: 'center',
+    marginTop: 20
+  },
+  textLogin: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    fontSize: 16,
+    textAlign: 'center'
+  }
+});
